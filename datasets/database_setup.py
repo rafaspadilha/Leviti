b364779306f928ca0ba202eab4373b8aae9ece73
Rafa <rafaspadilha@gmail.com>
2017-11-02 14:18:54 -0200

    database_setup updates

--- datasets/database_setup.py
+++ datasets/database_setup.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-
-from matplotlib import pyplot as plt
-
 import numpy as np
 import cv2
 import scipy
@@ -11,7 +8,6 @@ import sys
 import shutil
 
 # define default values from database
-IMG_SIZE = 224
 MIN_MATCH_COUNT = 1
 
 ignore = True
@@ -19,12 +15,12 @@ ignore = True
 OUTPUT = 'aligned'
 root   = '/work/rpadilha/AMOS/'
 
-def cv2keras(img):
-    return np.swapaxes(np.swapaxes(img, 1, 2), 0, 1)
 
 def register(original, new, transform = True):
     try:
         img = cv2.imread(new)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
 
         # Initiate SIFT detector
         sift = cv2.xfeatures2d.SIFT_create()
@@ -32,10 +28,10 @@ def register(original, new, transform = True):
         # find the keypoints and descriptors with SIFT
         kp1, des1 = sift.detectAndCompute(original, None)
 
-        if img is None:
+        if gray is None:
             return None
         else:
-            kp2, des2 = sift.detectAndCompute(img, None)
+            kp2, des2 = sift.detectAndCompute(gray, None)
 
         FLANN_INDEX_KDTREE = 0
         index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
@@ -57,6 +53,9 @@ def register(original, new, transform = True):
             if m.distance < 0.7 * n.distance:
                 good.append(m)
 
+        print "Number of good matches: ", len(good)
+
+
         # is it useful for us?
         if len(good) < MIN_MATCH_COUNT:
             print "Images are not sufficiently alike - %d/%d" % (len(good), MIN_MATCH_COUNT)
@@ -65,6 +64,7 @@ def register(original, new, transform = True):
             return None
 
         if len(good) > MIN_MATCH_COUNT and len(good) < MIN_MATCH_COUNT * 2:
+            print "matches > MIN and matches < 2* MIN"
             return img
 
         if transform and len(good) > MIN_MATCH_COUNT * 2:
@@ -85,12 +85,14 @@ def register(original, new, transform = True):
         else:
             res = img
 
-        # draw_params = dict(matchColor = (0,255,0), # draw matches in green color
-        #                    singlePointColor = None,
-        #                    matchesMask = matchesMask, # draw only inliers
-        #                    flags = 2)
+        draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                           singlePointColor = None,
+                           matchesMask = matchesMask, # draw only inliers
+                           flags = 2)
 
-        # img3 = cv2.drawMatches(original, kp1, img, kp2, good, None, **draw_params)
+        img3 = cv2.drawMatches(original, kp1, img, kp2, good, None, **draw_params)
+
+        cv2.imwrite(new + "_matches", img3)
 
         return res
 
@@ -117,6 +119,7 @@ def load_data(folder, min_obj, save):
         if model is None:
             m     = sub_n + '/' + img_f[0] # first image, taken as a model
             model = cv2.imread(m)
+            model = cv2.cvtColor(model, cv2.COLOR_BGR2GRAY)
 
             # get specs of image
             h = model.shape[0]
@@ -125,12 +128,12 @@ def load_data(folder, min_obj, save):
 	print "Model used: " + m
 	print "Model h,w: ", h, " ", w
 
-        for img in img_f:
+        for img in img_f[:4]:
             file =  sub_n + '/' + img
 
             try:
                 # get full image, following registration
-                data = register(model, file, False)
+                data = register(model, file, True)
 
                 # if registration went bad, simply ignore it
                 if data is None:
@@ -142,73 +145,8 @@ def load_data(folder, min_obj, save):
                 # check specs of image
                 if h != data.shape[0] or w != data.shape[1]:
                     continue
-		"""
-                # crop conners to image default size
-                for i in range(min(h/IMG_SIZE + 1, 2)):
-                    for j in range(min(w/IMG_SIZE + 1, 2)):
-                        cropped = data[(h - IMG_SIZE) * i:(h - IMG_SIZE) * i + IMG_SIZE,
-                                       (w - IMG_SIZE) * j:(w - IMG_SIZE) * j + IMG_SIZE, :]
 
-                        if not os.path.exists(save + str(num_img)):
-                            os.makedirs(save + str(num_img))
-
-                        label = save + str(num_img) + '/' + img
-
-                        avg_color = np.average(np.average(cropped, axis = 0), axis = 0)
-
-                        # sanity check for the image quality
-                        if ignore or not all (i >= 230 for i in avg_color):
-                            cv2.imwrite(label, cropped)
-
-                            num_img += 1
-                        else:
-                            del label
-                            del cropped
-
-                            print ('Incorrect image.')
-
-                # now, crop the center and be done with it!
-                if h/IMG_SIZE != 0 or w/IMG_SIZE != 0:
-                    cropped = data[h/2 - IMG_SIZE/2:h/2 + IMG_SIZE/2, 
-                                   w/2 - IMG_SIZE/2:w/2 + IMG_SIZE/2, :]
-
-                    if not os.path.exists(save + str(num_img)):
-                        os.makedirs(save + str(num_img))
-
-                    label = save + str(num_img) + '/' + img
-
-                    avg_color = np.average(np.average(cropped, axis = 0), axis = 0)
-
-                    # sanity check for the image quality
-                    if ignore or not all (i >= 230 for i in avg_color):
-                        cv2.imwrite(label, cropped)
-
-                        num_img += 1
-                    else:
-                        del label
-                        del cropped
-                            
-                        print ('Incorrect image.')
-
-                else:
-                    # in case the image is too small for any cropping, simply resize it
-                    cropped = scipy.misc.imresize(data, 
-                                                  (IMG_SIZE, IMG_SIZE), 
-                                                  interp='bilinear')
-
-                    if not os.path.exists(save + str(num_img)):
-                        os.makedirs(save + str(num_img))
-
-                    print ('Image is too small.')
-
-                    label = save + str(num_img) + '/' + img
-
-                    cv2.imwrite(label, cropped)
-
-                    num_img += 1
-		"""
-		cv2.imwrite(save + '/' + img, data)
-		
+                cv2.imwrite(save + '/' + img, data)
             except IOError as e:
                 print ('Could not read: ', img_f, ', skipping it!')
 
